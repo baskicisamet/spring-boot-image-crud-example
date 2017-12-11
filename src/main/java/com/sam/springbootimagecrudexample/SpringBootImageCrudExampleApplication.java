97bc33bf6e8f86cd497ea53bcbf4b13481433c2f
Charizard11 <baskicisamet@gmail.com>
2017-12-12 02:01:44 +0300

    Initial commit

---
+++ src/main/java/com/sam/springbootimagecrudexample/SpringBootImageCrudExampleApplication.java
@@ -0,0 +1,12 @@
+package com.sam.springbootimagecrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageCrudExampleApplication.class, args);
+	}
+}
